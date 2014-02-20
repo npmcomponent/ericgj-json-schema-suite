0ad424a8f8e7c68e6921822ce96d6a33bfd3f3cf
Toby Ho <airportyh@gmail.com>
2014-02-20 10:49:40 -0500

    Automatically rewritten for npm.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var isBrowser = require('is-browser')
-  , core = isBrowser ? require('json-schema-core') : require('json-schema-core-component')
-  , hyper = isBrowser ? require('json-schema-hyper') : require('json-schema-hyper-component')
-  , valid = isBrowser ? require('json-schema-valid') : require('json-schema-valid-component')
-  , Agent = isBrowser ? require('json-schema-agent') : require('json-schema-agent-component')
-  , request = require('superagent')
+var isBrowser = require('forbeslindesay-is-browser')
+  , core = isBrowser ? require('ericgj-json-schema-core') : require('json-schema-core-component')
+  , hyper = isBrowser ? require('ericgj-json-schema-hyper') : require('json-schema-hyper-component')
+  , valid = isBrowser ? require('ericgj-json-schema-valid') : require('json-schema-valid-component')
+  , Agent = isBrowser ? require('ericgj-json-schema-agent') : require('json-schema-agent-component')
+  , request = require('visionmedia-superagent')
   , Schema = core.Schema
   , Service = function(){ return request; }
 
